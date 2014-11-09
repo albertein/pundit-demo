6f4e85111f17285457511033cbe2c69921af63b6
Alberto Avila <albertoavila@gmail.com>
2014-11-09 16:38:16 -0600

    Aded home screen, instructions and sections menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  include Pundit
+  def app_sections
+    sections = [
+      {name: 'Students', path: students_path, type: Student},
+      {name: 'Teachers', path: teachers_path, type: Teacher},
+    ]
+    sections.select { |section| policy(section[:type]).index? }
+  end
 end
